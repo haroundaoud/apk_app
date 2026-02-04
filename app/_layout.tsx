395dadbca172905f6eb5399ff0cdb84d5fb809d3
haroundaoud <hama1daoud@gmail.com>
2026-02-04 16:58:40 +0100

    Implement user authentication and routing
    Added authentication state handling and routing logic.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,56 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { auth } from "./firebase/firebaseConfig";
 
 export default function RootLayout() {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    if (loading) return;
+
+    const inAuthGroup = segments[0] === "(auth)";
+
+    if (!user && !inAuthGroup) {
+      router.replace("/(auth)/login");
+    }
+
+    if (user && inAuthGroup) {
+      router.replace("/(tabs)");
+    }
+  }, [user, loading, segments]);
+
+  if (loading) return null; 
+
   return (
     <>
-    <StatusBar style="dark" />
-    
-    <Stack>
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="+not-found" options={{ title: "Oops!", headerStyle: { backgroundColor: "#25292e" }, headerTintColor: "#fff" }} />
-        
-
-    </Stack>
+      <StatusBar style="dark" />
+      <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="+not-found"
+          options={{
+            title: "Oops!",
+            headerStyle: { backgroundColor: "#25292e" },
+            headerTintColor: "#fff",
+          }}
+        />
+      </Stack>
     </>
   );
 }
